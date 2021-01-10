30b6775189e6fdce10cf2ab31eca9d1dc33fc074
--add <--add>
2021-01-10 22:34:51 +0530

    working on map

--- app/src/main/java/com/example/locationmanager/activities/HomeActivity.java
+++ app/src/main/java/com/example/locationmanager/activities/HomeActivity.java
@@ -1,7 +1,8 @@
 package com.example.locationmanager.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.locationmanager.R;
 
--- app/src/main/java/com/example/locationmanager/activities/RegisterActivity.java
+++ app/src/main/java/com/example/locationmanager/activities/RegisterActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.locationmanager.R;
 
 public class RegisterActivity extends AppCompatActivity {
