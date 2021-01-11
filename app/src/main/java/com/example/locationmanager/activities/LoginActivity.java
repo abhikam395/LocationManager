03545071a4c4baf828a08a179e05c9b90081e44e
--add <--add>
2021-01-11 22:12:48 +0530

    working on auth

--- app/src/main/java/com/example/locationmanager/activities/LoginActivity.java
+++ app/src/main/java/com/example/locationmanager/activities/LoginActivity.java
@@ -8,32 +8,78 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.locationmanager.R;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtRegister;
     private Button btnFb, btnGmail, btnContinue;
+    private TextInputEditText edtEmail;
+    private TextInputEditText edtPassword;
+    private String username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        init();
+    }
+
+    private void init(){
         txtRegister  = findViewById(R.id.login_text_register);
         btnFb = findViewById(R.id.login_btn_fb);
         btnGmail = findViewById(R.id.login_btn_g_mail);
         btnContinue = findViewById(R.id.login_btn_continue);
 
-        //navigate to RegisterActivity
-        txtRegister.setOnClickListener(v -> {
-            startActivity(new Intent(this, RegisterActivity.class));
-            finish();
-        });
+        edtEmail = findViewById(R.id.login_edt_email);
+        edtPassword = findViewById(R.id.login_edt_password);
 
-        btnContinue.setOnClickListener(v -> {
-            startActivity(new Intent(this, HomeActivity.class));
-        });
+        setListener();
+    }
+
+    private void setListener(){
+        txtRegister.setOnClickListener(this);
+        btnContinue.setOnClickListener(this);
+    }
+
+    private void setInputs(){
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+    }
+
+    private boolean isValidate(){
+        setInputs();
+
+        String email = this.email.trim(), password = this.password.trim();
+
+        if (email == null || !email.contains("@gmail.com"))
+            return  false;
+        else if(password == null || password.length() < 5)
+            return  false;
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_text_register: {
+                startActivity(new Intent(this, RegisterActivity.class));
+                finish();
+                break;
+            }
+            case R.id.login_btn_continue: {
+                if(isValidate()){
+                    startActivity(new Intent(this, HomeActivity.class));
+                    finish();
+                }
+                else
+                    Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/locationmanager/activities/RegisterActivity.java
+++ app/src/main/java/com/example/locationmanager/activities/RegisterActivity.java
@@ -3,33 +3,86 @@ package com.example.locationmanager.activities;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.example.locationmanager.R;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.example.locationmanager.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtLogin;
     private Button btnFb, btnGmail, btnContinue;
+    private TextInputEditText edtUsername;
+    private TextInputEditText edtEmail;
+    private TextInputEditText edtPassword;
+    private String username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        init();
+    }
+
+    private void init(){
         txtLogin  = findViewById(R.id.register_text_login);
         btnFb = findViewById(R.id.register_btn_fb);
         btnGmail = findViewById(R.id.register_btn_g_mail);
         btnContinue = findViewById(R.id.register_btn_continue);
 
-        //navigate to LoginActivity
-        txtLogin.setOnClickListener(v -> {
-            startActivity(new Intent(this, LoginActivity.class));
-            finish();
-        });
+        edtUsername = findViewById(R.id.register_edt_username);
+        edtEmail = findViewById(R.id.register_edt_email);
+        edtPassword = findViewById(R.id.register_edt_password);
 
-        btnContinue.setOnClickListener(v -> {
-            startActivity(new Intent(this, HomeActivity.class));
-        });
+        setListener();
+    }
+
+    private void setListener(){
+        txtLogin.setOnClickListener(this);
+        btnContinue.setOnClickListener(this);
+    }
+
+    private void setInputs(){
+        username = edtUsername.getText().toString();
+        email = edtEmail.getText().toString();
+        password = edtPassword.getText().toString();
+    }
+
+    private boolean isValidate(){
+        setInputs();
+
+        String name = this.username.trim(), email = this.email.trim(), password = this.password.trim();
+
+        if(name == null || name.length() < 5)
+            return false;
+        else if (email == null || !email.contains("@gmail.com"))
+            return  false;
+        else if(password == null || password.length() < 5)
+            return  false;
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register_text_login: {
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+                break;
+            }
+            case R.id.register_btn_continue: {
+                if(isValidate()){
+                    startActivity(new Intent(this, HomeActivity.class));
+                    finish();
+                }
+                else
+                    Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
     }
 }
